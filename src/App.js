c4ca3eea1ade01b9c2b5f82bfdca0a16ee0daa88
Roie Natan <roie.natan@gmail.com>
2021-08-18 14:37:10 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './index.scss';
+
+export default function Header() {
+  return (
+    <div className="header-wrapper">
+      <div>LOGO</div>
+      <div>Whitepaper</div>
+      <div>FAQ</div>
+    </div>
+  )
+}
