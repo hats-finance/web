0a58a388d733b1183fc0e7c3e56bef0749eb9409
Roie Natan <roie.natan@gmail.com>
2022-02-05 00:29:04 -0500

    auto scroll tabs wrapper on click

--- src/components/NFT/components/TabsController/TabsController.tsx
+++ src/components/NFT/components/TabsController/TabsController.tsx
@@ -1,4 +1,7 @@
+import { useContext, useRef } from "react";
 import { useTranslation } from "react-i18next";
+import { LayoutContext } from "../../../../App";
+import { ScreenSize } from "../../../../constants/constants";
 import { Tab } from "../../NFT";
 import "./index.scss";
 
@@ -9,12 +12,29 @@ interface IProps {
 
 export default function TabsController({ currentTab, setCurrentTab }: IProps) {
   const { t } = useTranslation();
+  const tabsWrapperElement = useRef<any>();
+  const { screenSize } = useContext(LayoutContext);
+
+  const handleClick = (tab: Tab) => {
+    setCurrentTab(tab);
+    if (screenSize === ScreenSize.Mobile) {
+      const maxLeftScroll = tabsWrapperElement.current?.clientWidth - tabsWrapperElement.current?.scrollWidth;
+      switch (tab) {
+        case Tab.Airdrop:
+          return tabsWrapperElement.current.scrollLeft = maxLeftScroll;
+        case Tab.Collections:
+          return tabsWrapperElement.current.scrollLeft = -maxLeftScroll / 2;
+        case Tab.ForArtists:
+          return tabsWrapperElement.current.scrollLeft = -maxLeftScroll;
+      }
+    }
+  }
 
   return (
-    <div className="tabs-controller-wrapper">
-      <div className={`tab ${currentTab === Tab.Airdrop && "selected"}`} onClick={() => setCurrentTab(Tab.Airdrop)}>{t("NFTRouth.TabController.airdrop")}</div>
-      <div className={`tab ${currentTab === Tab.Collections && "selected"}`} onClick={() => setCurrentTab(Tab.Collections)}>{t("NFTRouth.TabController.collections")}</div>
-      <div className={`tab ${currentTab === Tab.ForArtists && "selected"}`} onClick={() => setCurrentTab(Tab.ForArtists)}>{t("NFTRouth.TabController.forArtists")}</div>
-    </div>
+    <div ref={tabsWrapperElement} className="tabs-controller-wrapper">
+      <div className={`tab ${currentTab === Tab.Airdrop && "selected"}`} onClick={() => handleClick(Tab.Airdrop)}>{t("NFTRouth.TabController.airdrop")}</div>
+      <div className={`tab ${currentTab === Tab.Collections && "selected"}`} onClick={() => handleClick(Tab.Collections)}>{t("NFTRouth.TabController.collections")}</div>
+      <div className={`tab ${currentTab === Tab.ForArtists && "selected"}`} onClick={() => handleClick(Tab.ForArtists)}>{t("NFTRouth.TabController.forArtists")}</div>
+    </div >
   )
 }
