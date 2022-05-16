24fc947746343adc264d981ea22626a56a066a35
mwogates <michaelwonggates@gmail.com>
2022-05-16 12:48:33 -0700

    update gradients

--- src/components/Landing/components/Welcome/Welcome.tsx
+++ src/components/Landing/components/Welcome/Welcome.tsx
@@ -7,15 +7,17 @@ export default function Welcome() {
 
   return (
     <div className="section welcome-wrapper">
-      <div className="section-content">
-        <div className="section-text-wrapper">
-          <div className="hats-finance">{t("Landing.Welcome.HATS.FINANCE")}</div>
-          <div className="section-title">{t("Landing.Welcome.title")}</div>
-          <div className="section-description">{t("Landing.Welcome.description")}</div>
+      <div className="gradient-section">
+        <div className="section-content">
+          <div className="section-text-wrapper">
+            <div className="hats-finance">{t("Landing.Welcome.HATS.FINANCE")}</div>
+            <div className="section-title">{t("Landing.Welcome.title")}</div>
+            <div className="section-description">{t("Landing.Welcome.description")}</div>
+          </div>
+          <img src={require("../../../../assets/images/landing/welcome.svg").default} className="section-image" alt="all users" />
         </div>
-        <img src={require("../../../../assets/images/landing/welcome.svg").default} className="section-image" alt="all users" />
+        <button className="button start" onClick={() => document.getElementById("startStage")?.scrollIntoView({ behavior: "smooth" })}>{t("Landing.Welcome.start")}<img src={require("../../../../assets/icons/landing/down-arrow.svg").default} alt="arrow" /></button>
       </div>
-      <button className="button start" onClick={() => document.getElementById("startStage")?.scrollIntoView({ behavior: "smooth" })}>{t("Landing.Welcome.start")}<img src={require("../../../../assets/icons/landing/down-arrow.svg").default} alt="arrow" /></button>
       <div className="moto">{t("Landing.Welcome.moto")}</div>
     </div>
   )
