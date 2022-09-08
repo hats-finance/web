124660ffdedd9d4cbd4c3ffb264504b13204aba4
Roie Natan <roie.natan@gmail.com>
2022-09-08 20:31:40 +0800

    more updates

--- src/components/NFT/components/EmbassyCollection/EmbassyCollection.tsx
+++ src/components/NFT/components/EmbassyCollection/EmbassyCollection.tsx
@@ -2,14 +2,31 @@ import { useTranslation } from "react-i18next";
 import EmbassyNftCard from "./components/EmbassyNftCard/EmbassyNftCard";
 import "./index.scss";
 
-const FEATURED_NFTS = ["892", "342", "427", "374", "317", "807", "311", "852", "318", "801", "153", "19"];
+const FEATURED_NFTS = [
+  {
+    project: "hats",
+    role: "protector",
+    tier: "low",
+  },
+  {
+    project: "fujidao",
+    role: "mother",
+    tier: "medium",
+  },
+  {
+    project: "dxdao",
+    role: "hacker",
+    tier: "high",
+  }
+]
+
 const ARTIST_LINK = "https://www.facebook.com/Rafahu/";
 
 export default function EmbassyCollection() {
   const { t } = useTranslation();
 
-  const nfts = FEATURED_NFTS.map(nft => {
-    return <EmbassyNftCard key={nft} index={nft} />;
+  const nfts = FEATURED_NFTS.map((nft, index) => {
+    return <EmbassyNftCard key={index} nftData={nft} />;
   })
 
   return (
--- src/components/NFT/components/EmbassyCollection/components/EmbassyNftCard/EmbassyNftCard.tsx
+++ src/components/NFT/components/EmbassyCollection/components/EmbassyNftCard/EmbassyNftCard.tsx
@@ -3,27 +3,35 @@ import { IPFS_PREFIX } from "../../../../../../constants/constants";
 import "./index.scss";
 
 interface IProps {
-  index: string;
+  nftData: { project: string, role: string, tier?: string };
 }
 
-const IPFS_NFT_COLLECTION = "QmSiPFLfYwodihG94ASaiWJuQ6uLUXkz8p8kvoCTv8KraP";
+const Tiers = {
+  "low": 1,
+  "medium": 2,
+  "high": 3
+}
 
-export default function EmbassyNftCard({ index }: IProps) {
+const IPFS_NFT_COLLECTION = "QmdyyJWXrD8texwRW41sCxGzAGcmnWrWPRSx58EFk3KA3S"
+
+export default function EmbassyNftCard({ nftData }: IProps) {
   const { t } = useTranslation();
-  
+
   return (
     <div className="embassy-nft-card-wrapper">
-      <img src={`${IPFS_PREFIX}/${IPFS_NFT_COLLECTION}/${index}.png`} alt="nft" />
+      <img src={`${IPFS_PREFIX}/${IPFS_NFT_COLLECTION}/${nftData.project}/${nftData.role}/${nftData.tier}.jpg`} alt="nft" />
       <div className="embassy-nft-card__info-container">
-        <div className="embassy-nft-card__info-title">NFT TITLE</div>
+        <div className="embassy-nft-card__info-title">{nftData.role}</div>
         <div className="embassy-nft-card__info-element">
           <div className="embassy-nft-card__info-element-title">{t("NFTRouth.EmbassyCollection.EmbassyNftCard.vault-embassy")}</div>
-          <div className="embassy-nft-card__info-element-value">VAULT NAME</div>
+          <div className="embassy-nft-card__info-element-value">{nftData.project}</div>
         </div>
-        <div className="embassy-nft-card__info-element">
-          <div className="embassy-nft-card__info-element-title">{t("NFTRouth.EmbassyCollection.EmbassyNftCard.tier")}</div>
-          <div className="embassy-nft-card__info-element-value">TIER {t("NFTRouth.EmbassyCollection.EmbassyNftCard.tier-text")}</div>
-        </div>
+        {nftData.tier && (
+          <div className="embassy-nft-card__info-element">
+            <div className="embassy-nft-card__info-element-title">{t("NFTRouth.EmbassyCollection.EmbassyNftCard.tier")}</div>
+            <div className="embassy-nft-card__info-element-value">{(Tiers as any)[nftData.tier]} {t("NFTRouth.EmbassyCollection.EmbassyNftCard.tier-text")}</div>
+          </div>
+        )}
       </div>
     </div>
   )
