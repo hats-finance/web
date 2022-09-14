918491ad3082d0a98f9e157a5a21b7811d186585
Roie Natan <roie.natan@gmail.com>
2022-09-14 19:31:37 +0700

    don't show scolling bars on windows ; hide 'new' label when going into nft path from anywhere

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,28 @@
 import './index.scss';
 import { useTranslation } from 'react-i18next';
 import { LayoutContext } from '../../App';
-import { useCallback, useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { COOKIES_POLICY, DAPP_LINK, GIT_BOOK, LocalStorage, RoutePaths, ScreenSize, SocialLinks, TERMS_OF_USE } from '../../constants/constants';
 import Menu from "./Menu";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
   const { t } = useTranslation();
   const { screenSize } = useContext(LayoutContext);
   const [toggleMenu, setToggleMenu] = useState(false);
   const [hasSeenNft, setHasSeenNft] = useState(localStorage.getItem(LocalStorage.NewNft));
+  const location = useLocation();
 
-  const handleNftClick = useCallback(() => {
-    localStorage.setItem(LocalStorage.NewNft, "1");
-    setHasSeenNft("1");
-    setToggleMenu(false);
-  }, [])
+  useEffect(() => {
+    if (location.pathname.includes(RoutePaths.NFT)) {
+      setHasSeenNft("1");
+      localStorage.setItem(LocalStorage.NewNft, "1");
+    }
+  }, [location])
 
   const links = <div className="links-wrapper">
     <NavLink to={RoutePaths.Gamification} onClick={() => setToggleMenu(false)} activeClassName="selected">{t("Gamification.Gamification")}</NavLink>
-    <NavLink to={RoutePaths.NFT} onClick={handleNftClick} className="links__nft" activeClassName="selected">
+    <NavLink to={RoutePaths.NFT} onClick={() => setToggleMenu(false)} className="links__nft" activeClassName="selected">
       {!hasSeenNft && (
         <div className="links__nft-new">
           <div className="links__dot" />
