dc2321b8e9899e05105dc8090560a0b951825294
Roie Natan <roie.natan@gmail.com>
2022-09-20 14:37:32 +0700

    updates

--- src/components/Gamification/components/Challenges/CTFS/CTF2.tsx
+++ src/components/Gamification/components/Challenges/CTFS/CTF2.tsx
@@ -1,7 +1,8 @@
 import { useTranslation } from "react-i18next";
 import "../../../index.scss";
 
-const CTF2_LINK = "https://github.com/hats-finance/games";
+const CTF2_LINK = "https://github.com/hats-finance/vault-game";
+const FAQ_LINK = "https://docs.hats.finance/ctf/faq";
 
 export default function CTF2(){
   const { t } = useTranslation();
@@ -19,6 +20,7 @@ export default function CTF2(){
       <section>
         <div className="title">{t("ChallengesRoute.Challenges.CTF2.title-3")}</div>
         <div>{t("ChallengesRoute.Challenges.CTF2.text-3")}</div>
+        <a className="ctf-2__faq-link" target="_blank" rel="noopener noreferrer" href={FAQ_LINK}>FAQ</a>
       </section>
       <button className="button ctf-2-button" onClick={() => window.open(CTF2_LINK)}>{t("ChallengesRoute.Challenges.CTF2.button-1")}</button>
     </div>
